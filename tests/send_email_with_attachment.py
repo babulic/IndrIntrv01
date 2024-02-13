dfaad2625044c85c7f2cf0b8a22bc1116d1f2f8e
ivan <ivan.babulic@gmail.com>
2024-02-13 23:37:01 +0100

    fixing login logout 01

--- tests/send_email_with_attachment.py
+++ tests/send_email_with_attachment.py
@@ -1,5 +1,14 @@
 from pytest_bdd import scenario, given, when, then
 
+
+@scenario('../features/send_email_with_attachment.feature', 'Send an email with attachment')
+def test_send_email_with_attachment():
+    """
+    Scenario: Send an email with attachment
+    """
+    pass
+
+
 @when('I attach a file')
 def attach_file(page):
     # Assuming there's an input for file uploads with the ID 'file-upload'
